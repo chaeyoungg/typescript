5275debeeead9c952d372ca4eac62accc07131b6
Kilyong Jeong <uzoolove@gmail.com>
2024-07-15 13:45:06 +0900

    ✨

--- workspace-ins/js/src/App.jsx
+++ workspace-ins/js/src/App.jsx
@@ -3,7 +3,6 @@ import router from '@/routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
-import { PropagateLoader } from 'react-spinners';
 import Spinner from '@/components/Spinner';
 
 
